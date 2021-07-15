5653bd621577d36b97075eaf44daa5896d41acf9
TayaHatum <21taya21@gmail.com>
2021-07-15 21:09:12 +0300

    added models

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,5 +1,7 @@
+import models.User;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,6 +18,19 @@ public void precondition(){
 
         app.userHelper().openLoginForm();
         app.userHelper().fillLoginForm("noa@gmail.com","Nnoa12345$");
+        app.userHelper().submitLogin();
+        app.userHelper().pause(3000);
+        String loginS=app.userHelper().getText(By.xpath("//div[@class='dialog-container']//h2"));
+        app.userHelper().clickOkButton();
+        Assert.assertEquals(loginS,"Logged in success");
+
+    }
+    @Test
+    public void loginTestPositiveDto(){
+        User user=new User().withEmail("noa@gmail.com").withPassword("Nnoa12345$");
+
+        app.userHelper().openLoginForm();
+        app.userHelper().fillLoginForm(user);
         app.userHelper().submitLogin();
         app.userHelper().pause(3000);
         String loginS=app.userHelper().getText(By.xpath("//div[@class='dialog-container']//h2"));
